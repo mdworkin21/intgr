5e1942e7eaa1fd08adbb30c7d9a4d600bc6ed7ee
Matt Dworkin <mdworkin21@gmail.com>
2019-02-18 15:35:00 -0500

    basic structure getting set up

---
+++ client/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import store from './redux/store'
+
+const App = () => {
+  return <h1>HELLO</h1>
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const morgan = require('morgan')
+const app = express()
+// const db = require('./db/database')
+
+
+//Logging MiddleWare
+app.use(morgan('dev'))
+
+//BodyParsing MiddleWare
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+//Static Middleware
+app.use(express.static(path.join(__dirname, '..', '/client/public')))
+
+//Api Routes
+
+// Redirects to homepage when no API reqs match
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '/client/public/'))
+})
+
+//Handles 500 Errs
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal Server Err. Whoops!')
+})
+
+
+module.exports = app
